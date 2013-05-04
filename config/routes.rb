22eb52796df9f3daea1fa75e669b467baf16059e
Ryan Patrick Kirlin <rpkirlin@gmail.com>
2013-05-04 12:13:30 -0400

    Added Gemfile for sign up encryption

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,18 @@ RailsTest::Application.routes.draw do
 
   get "sessions/destroy"
 
-root :to => "static_pages#welcome"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+
+root :to => "users#new"
 #root is just your root url. It's saying go to the static_pages and look for welcome
 
 #get "/" => "static_pages#welcome"
 #this is what you'll do for other roots beyond the root
+
+  resources :users
+  resources :sessions
 
 post "create" => "static_pages#create"
 
